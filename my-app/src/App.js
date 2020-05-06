5de6f35590120e576b8f0eeed82c64a7d585ad13
Christian Coulter <95coulter@gmail.com>
2020-05-05 18:35:10 -0600

    Transferred files from wrong directory to correct forked repo. Finished MVP. Will work on Stretch next.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,104 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import 'semantic-ui-css/semantic.min.css';
+import {Card, Icon, Image} from 'semantic-ui-react';
+import UserCard from './UserCard.js';
 
-function App() {
+
+export default class App extends React.Component {
+  
+  constructor() {
+    super()
+    this.state = {
+      me: [],
+      avatar: [],
+      location: [],
+      bio: [],
+      repos: [],
+      cardInfo: [],
+      followersInfo: [],
+      Users: [],
+    }
+  }
+
+
+  componentDidMount() {
+    fetch('https://api.github.com/users/james-coulter')
+    .then((res) => res.json())
+    .then(data => {
+      // console.log('User Data', data)
+      this.setState({ name: data.name})
+      this.setState({location: data.location})
+      this.setState({ avatar: data.avatar_url})
+      this.setState({ repos: data.public_repos})
+      this.setState({ bio: data.bio})
+    })
+    .catch(err => console.log('Oh no!', err))
+
+    axios
+      .get(
+        "https://api.github.com/users/james-coulter/followers"
+      )
+      .then((res) => {
+        this.setState({
+          followersInfo: res.data,
+        });
+        console.log("followers", res);
+        res.data.forEach((person) => {
+          axios
+            .get(
+              `https://api.github.com/users/${person.login}`
+            )
+            .then((res) => {
+              let joined = this.state.Users.concat(res.data);
+              this.setState({
+                Users: joined,
+              });
+              console.log("follower", res);
+            });
+        });
+      })
+      .catch((err) => console.log(err));
+  }
+
+
+
+
+  render () {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+      <h1>Github UserCard</h1>
       </header>
+
+      <div className="meCard">
+        <Card>
+          <Image src={this.state.avatar} wrapped ui={false} />
+          <Card.Content>
+            <Card.Header>{this.state.name}</Card.Header>
+            <Card.Meta>
+              <span className='date'>{this.state.location}</span>
+              </Card.Meta>
+              <Card.Description>
+              {this.state.bio}
+            </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+            <a>
+              <Icon name='user' />
+              Repos: {this.state.repos}
+            </a>
+          </Card.Content>
+        </Card>
+      </div>
+      <UserCard Users={this.state.Users}/>
+
+
     </div>
   );
 }
+}
 
-export default App;
---
+++ my-app/src/UserCard.js
@@ -0,0 +1,38 @@
+import React from 'react';
+// import './Card.css';
+import 'semantic-ui-css/semantic.min.css';
+import {Card, Icon, Image} from 'semantic-ui-react';
+
+function UserCard(props) {
+const {Users} = props;
+
+
+  return (
+    <div className="userCard">
+    {Users.map((follower, index) => {
+      return (
+        <Card className="indCard" key={index}>
+        <Image src={follower.avatar_url} wrapped ui={false} />
+        <Card.Content>
+          <Card.Header>{follower.name}</Card.Header>
+          <Card.Meta>
+            <span className='date'>{follower.location}</span>
+            </Card.Meta>
+            <Card.Description>
+            {follower.bio}
+          </Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+          <a>
+            <Icon name='user' />
+            Repos: {follower.public_repos}
+          </a>
+        </Card.Content>
+      </Card>
+      )
+    })}
+    </div>
+  )
+  }
+
+export default UserCard;
